9b2310d030a35299b8877fe1dde499b622dac93a
KumaresanK <tkkumaresan@gmail.com>
2020-10-11 20:27:16 +0530

    postgres integrated

---
+++ src/main/java/com/kd/spring_boot_sample_sts/App.java
@@ -0,0 +1,13 @@
+package com.kd.spring_boot_sample_sts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/kd/spring_boot_sample_sts/AppTest.java
@@ -0,0 +1,20 @@
+package com.kd.spring_boot_sample_sts;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
